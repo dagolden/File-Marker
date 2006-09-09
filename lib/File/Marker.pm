e26a178895e9ed6521858817e39906a7a4cea090
David Golden <dagolden@cpan.org>
2006-09-09 16:51:05 +0000

    r3869@vesta (orig r395):  david | 2006-06-09 02:42:58 -0400  full basic functionality working and tested
    git-svn-id: https://dagolden.googlecode.com/svn/File-Marker/trunk@407 dfce27d5-b31c-0410-bb09-030b4413eeba

--- lib/File/Marker.pm
+++ lib/File/Marker.pm
@@ -1,17 +1,15 @@
 package File::Marker;
+
+$VERSION = "0.10";
+@ISA = qw( IO::File );
+
 use strict;
 use warnings;
 use Carp;
+use IO::File;
+use Scalar::Util qw( refaddr );
 
-BEGIN {
-    use Exporter ();
-    use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = "0.10";
-    @ISA         = qw (Exporter);
-    @EXPORT      = qw ();
-    @EXPORT_OK   = qw ();
-    %EXPORT_TAGS = ();
-}
+my %marks_for = ();
 
 #--------------------------------------------------------------------------#
 # main pod documentation 
@@ -36,17 +34,121 @@ Description...
 
 Usage...
 
+=head2 new
+
 =cut
 
 sub new {
-    my ($class, $parameters) = @_;
+    my $class = shift;
     
-    croak "new() can't be invoked on an object"
-        if ref($class);
-        
-    my $self = bless ({ }, $class);
+    my $self = IO::File->new();
+    bless $self, $class;
+    $self->open( @_ ) if @_;
     return $self;
 }
+
+
+#--------------------------------------------------------------------------#
+# open()
+#--------------------------------------------------------------------------#
+
+=head2 open
+
+ $rv = open();
+
+Description of open...
+
+=cut
+
+sub open {
+    my $self = shift;
+    $marks_for{ refaddr $self } = {};
+    $self->SUPER::open( @_ );
+    $marks_for{ refaddr $self }{'LAST'} = $self->getpos;
+}
+
+
+#--------------------------------------------------------------------------#
+# set_marker()
+#--------------------------------------------------------------------------#
+
+=head2 set_marker
+
+ $rv = set_marker();
+
+Description of set_marker...
+
+=cut
+
+sub set_marker {
+    my ($self, $mark) = @_;
+    
+    croak "Can't set marker on closed filehandle"
+        if ! $self->opened;
+
+    croak "Can't set special marker 'LAST'" 
+        if $mark eq 'LAST';
+        
+    my $position = $self->getpos;
+    croak "Couldn't set marker '$mark': couldn't locate position in file"
+        if ! defined $position;
+    
+    $marks_for{ refaddr $self }{$mark} = $self->getpos;
+    
+    return 1;
+}
+
+#--------------------------------------------------------------------------#
+# goto_marker()
+#--------------------------------------------------------------------------#
+
+=head2 goto_marker
+
+ $rv = goto_marker();
+
+Description of goto_marker...
+
+=cut
+
+sub goto_marker {
+    my ($self, $mark) = @_;
+    
+    croak "Can't goto marker on closed filehandle"
+        if ! $self->opened;
+
+    croak "Unknown file marker '$mark'"
+        if ! exists $marks_for{refaddr $self}{$mark};
+    
+    my $old_position = $self->getpos; # save for LAST
+    
+    my $rc = $self->setpos( $marks_for{refaddr $self}{$mark} );
+    croak "Couldn't goto marker '$mark': could not seek to location in file"
+        if ! defined $rc;
+    
+    $marks_for{ refaddr $self }{ 'LAST' } = $old_position;
+
+    return 1;
+}
+
+
+#--------------------------------------------------------------------------#
+# markers()
+#--------------------------------------------------------------------------#
+
+=head2 markers
+
+ $rv = markers();
+
+Description of markers...
+
+=cut
+
+sub markers {
+    my $self = shift;
+    return keys %{ $marks_for{ refaddr $self } };
+}
+
+
 
 1; #this line is important and will help the module return a true value
 __END__
